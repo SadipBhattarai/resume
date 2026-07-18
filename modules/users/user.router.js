60e2479f9a702c08cd6dd744f17dff448e8623e5
SadipBhattarai <sadipbhattarai3@gmail.com>
2026-07-18 21:10:09 +0545

    Added register and login functions

--- modules/users/user.router.js
+++ modules/users/user.router.js
@@ -1,3 +1,17 @@
 const router = require("express").Router();
-
-module.exports= router;
+const userController = require("../users/user.controller");
+router.post("/login", async (req, res, next) => {
+  try {
+  } catch (error) {
+    next(error);
+  }
+});
+router.post("/register", async (req, res, next) => {
+  try {
+    const result = await userController.register(req.body);
+    req.json({ data: `User Registered Sucessfully` });
+  } catch (error) {
+    next(error);
+  }
+});
+module.exports = router;
--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,11 @@
 const router = require("express").Router();
 
 const userRouter = require("../modules/users/user.router");
-router.get("/", (req,res, next) => {
+router.get("/", (req, res, next) => {
   try {
     res.json({ data: `API is woking properly` });
   } catch (error) {
-    next(error)
+    next(error);
   }
 });
 router.use("/api/v1/users", userRouter);
