77379264bd7e01c833ae5170914d81bbee8275e6
SadipBhattarai <sadipbhattarai3@gmail.com>
2026-07-18 17:47:42 +0545

    Initial Setup

---
+++ index.js
@@ -0,0 +1,26 @@
+require("dotenv").config();
+const express = require("express");
+const morgan = require("morgan");
+const app = express();
+const mongoose = require("mongoose");
+const indexRouter = require("./routes");
+const PORT = process.env.PORT || 8002;
+
+mongoose
+  .connect(process.env.DB_URL)
+  .then(console.log(`Database Connected Sucessfully`))
+  .catch((e) => console.log("Database error", e.toString()));
+
+app.use(express.json());
+app.use(morgan("tiny"));
+
+app.use("/", indexRouter);
+
+app.use((error, req, res, next) => {
+  const errMsg = error.toString() || `Something went Wrong.`;
+  res.status(500).json({ data: null, error: errMsg });
+});
+
+app.listen(PORT, () =>
+  console.log(`Application is running sucessfully on http://localhost:${PORT}`),
+);
